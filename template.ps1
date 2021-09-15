ad552b5f3cc12cc2b079a610e681d88fdcc5e8b6
kohnson <scp100j@gmail.com>
2021-09-15 14:11:25 -0400

    Added Write-Two function
    Writes text without a name, allowing for multiple lines of text without
    repeatedly stating a name.

--- template.ps1
+++ template.ps1
@@ -4,25 +4,43 @@
 # Lets go
 # Writing speech from a character
 
-	# init
-	$Random = New-Object System.Random
+# init
+$Random = New-Object System.Random
 
 function Write-Say {
 	param (
-		[Parameter (Mandatory = $true)]
+		[Parameter (Mandatory = $true)] # Name param
 		[string]
 		$name,
 
-		[Parameter (Mandatory = $true)]
+		[Parameter (Mandatory = $true)] # Text param
 		[string]
 		$txt
 		)
+
 	Write-Host -ForegroundColor Cyan -NoNewLine "$name`: "
 	$txt -split "" |
 		ForEach-Object{
 			Write-Host $_ -NoNewLine -ForegroundColor Yellow
 			Start-Sleep -milliseconds $(1 + $Random.Next(200))
 		}
+	Write-Output ""
+}
+
+function Write-Two {
+	param (
+		[Parameter (Mandatory = $true, ParameterSetName = "Text")] # Text param
+		[string]
+		$txt
+		)
+	$txt -split "" |
+		ForEach-Object {
+			Write-Host $_ -NoNewLine -ForegroundColor Yellow
+			Start-Sleep -milliseconds $(1 + $Random.Next(100))
+		}
+		Write-Output ""
 }
 
 Write-Say -name "Mech" -txt "One Two Three Four Five"
+Write-Two -txt "This is a secondary line in which i discuss my moonshine."
+
