0345b9b98b8660952b3eb75c5fd1eafae7bec733
kohnson <scp100j@gmail.com>
2021-09-15 13:12:42 -0400

    Started work on template file
    Write-Say (writing speech from a character) is finished

---
+++ template.ps1
@@ -0,0 +1,28 @@
+#!/usr/bin/pwsh
+## A script for figuring out how to do various mechanics in the game
+
+# Lets go
+# Writing speech from a character
+
+	# init
+	$Random = New-Object System.Random
+
+function Write-Say {
+	param (
+		[Parameter (Mandatory = $true)]
+		[string]
+		$name,
+
+		[Parameter (Mandatory = $true)]
+		[string]
+		$txt
+		)
+	Write-Host -ForegroundColor Blue -NoNewLine "$name`: "
+	$txt -split "" |
+		ForEach-Object{
+			Write-Host $_ -NoNewLine -ForegroundColor Yellow
+			Start-Sleep -milliseconds $(1 + $Random.Next(200))
+		}
+}
+
+Write-Say -name "Mech" -txt "One Two Three Four Five"
