02be09e16710a365de201696f7f93c7dbb2f2de0
kohnson <scp100j@gmail.com>
2021-11-22 21:09:45 -0500

    Merge modules folder into one file

--- powershell/PCHmodules.ps1
+++ powershell/PCHmodules.ps1
@@ -1,10 +1,11 @@
-#!/usr/bin/pwsh
-# Save the game and pick up where you left off.
-# Save points will be separated by scene.
-# Scenes will each be defined as a function
+#!/usr/bin/env pwsh
+## Modules for Polcompistan High: PowerShell Edition
+## If you are reading this, head over to https://www.cnet.com/health/nutrition/best-weight-loss-programs/
 
-# actual function
-function Save-Status {
+# init
+$Random = New-Object System.Random
+
+function Save-Status { # Save the game and pick up where you left off.
 	Write-Host -BackgroundColor DarkBlue "Save Game"
 	Write-Host -ForegroundColor Green  "Use an existing name to overwrite a save, or write a new name to create another slot."
 	Write-Host -ForegroundColor Green "Save names are not case-sensitive."
@@ -16,13 +17,13 @@ function Save-Status {
 	# At this point, you're just going to write your chapter and whatever variables to $SaveFile
 	# All variables will have their name stored in .\vars.db
 	# For example:
-	"#!/usr/bin/pwsh`n## MechanicalPhoenix Dating Simulator save file`n## DO NOT EDIT`n" > $SaveFile # Only one ">" is used so the older version is overwritten completely
+	"#!/usr/bin/pwsh`n## MechanicalPhoenix Dating Simulator save file`n## DO NOT EDIT`n" > $SaveFile # Overwrite previous iteration
 	ForEach ($line in Get-Content .\vars.db) {	## btw, the syntax for vars.db will be like so:
 		"$line = $($line)" >> $SaveFile		# $chapter
 	}						# $secondvar
 }							## and so on...
 
-function Restore-Status {
+function Restore-Status { # Pick up where you left off
 	Write-Host -BackgroundColor DarkBlue "Restore Saved Game"
 	Write-Host -ForegroundColor Green "Select a save to restore, or leave blank to start fresh."
 	$(Get-ChildItem -Name .\saves) -Replace ".ps1", ""
@@ -33,3 +34,37 @@ function Restore-Status {
 	# Pick up from a chapter start
 	Invoke-Expression $chapter
 }
+
+function Write-Say {
+	param (
+		[Parameter (Mandatory = $true)] # Name param
+		[string]
+		$name,
+
+		[Parameter (Mandatory = $true)] # Text param
+		[string]
+		$txt
+		)
+
+	Write-Host -ForegroundColor Cyan -NoNewLine "$name`: "
+	$txt -split "" |
+		ForEach-Object{
+			Write-Host $_ -NoNewLine
+			Start-Sleep -Milliseconds $(1 + $Random.Next(200))
+		}
+}
+
+function Write-Two {
+	param (
+		[Parameter (Mandatory = $true, ParameterSetName = "Text")] # Text param
+		[string]
+		$txt
+		)
+	$txt -split "" |
+		ForEach-Object {
+			Write-Host $_ -NoNewLine
+			Start-Sleep -milliseconds $(1 + $Random.Next(100))
+		}
+		Write-Output ""
+}
+
--- powershell/modules/README.md
+++
@@ -1,7 +0,0 @@
-Module Scripts
-================
-
-These scripts each contain a function or two that acts as a mechanic in the game.
-
-- Write-Say : Write speech from a character.
-- Save-Status : Save the game at a save point and pick up where you left off later.
